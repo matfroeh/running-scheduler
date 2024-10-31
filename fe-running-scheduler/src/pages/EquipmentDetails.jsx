04524a8d2e0f9d7a3bfff09ad30987363589afa6
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-10-31 10:56:03 +0100

    uploading images into equipment details page, some style fixing still needed

--- fe-running-scheduler/src/pages/EquipmentDetails.jsx
+++ fe-running-scheduler/src/pages/EquipmentDetails.jsx
@@ -1,5 +1,5 @@
 import { CardModal } from "@/components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams, useOutletContext, useNavigate } from "react-router-dom";
 import {
   getEquipmentById,
@@ -9,14 +9,21 @@ import {
 import { useAuth } from "@/context";
 import { toast } from "react-toastify";
 import dayjs from "dayjs";
+import axios from "axios";
 
 const EquipmentDetails = () => {
   const { equipmentId } = useParams();
+  const imgInputRef = useRef(null);
+
   const navigate = useNavigate();
   const [formData, setFormData] = useState("");
   const { user } = useAuth();
   const { setEquipmentList } = useOutletContext();
- 
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageId, setImageId] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [images, setImages] = useState(null);
+
   const formatDate = (date) => {
     return dayjs(date).format("YYYY-MM-DD");
   };
@@ -25,9 +32,44 @@ const EquipmentDetails = () => {
     const fetchEquipment = async () => {
       const data = await getEquipmentById(user.userId, equipmentId);
       setFormData(data);
+
+      const imageId = data.image;
+      const imageData = await axios.get(
+        `http://localhost:3000/uploads/${imageId}`
+      );
+      if (imageData) {
+        setImages(imageData.data);
+      }
     };
     fetchEquipment();
   }, [equipmentId, user.userId]);
+
+  console.log(formData.image);
+
+  // useEffect(() => {
+  //   // Fetch images from the server when the component mounts
+  //   const fetchImage = async (imageId) => {
+  //     try {
+  //       const response = await axios.get(
+  //         `http://localhost:3000/uploads/${imageId}`
+  //       );
+  //       setImages(response.data);
+  //     } catch (error) {
+  //       toast.error("Failed to load image:", error);
+  //     }
+  //   };
+  //   fetchImage(formData.image);
+  // },[]);
+
+  const arrayBufferToBase64 = (buffer) => {
+    let binary = "";
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -42,21 +84,57 @@ const EquipmentDetails = () => {
       "Are you sure you want to delete this equipment?"
     );
     if (!confirmDelete) return;
-    await deleteEquipmentFromUserList(user.userId, equipmentId);  
-    setEquipmentList((prev) => prev.filter((equipment) => equipment._id !== equipmentId));
+    await deleteEquipmentFromUserList(user.userId, equipmentId);
+    setEquipmentList((prev) =>
+      prev.filter((equipment) => equipment._id !== equipmentId)
+    );
     toast.success("Equipment deleted successfully");
     navigate(-1);
     // deleteEquipment(user.userId, equipmentId);
   };
 
-
   const update = async () => {
     // ToDo: Error handling
     const updatedEquipmentData = { ...formData };
+    if (imageId) updatedEquipmentData.image = imageId;
     await updateEquipment(user.userId, equipmentId, updatedEquipmentData);
-    setEquipmentList((prev) => prev.map((equipment) => (equipment._id === equipmentId ? updatedEquipmentData : equipment)));
+    setEquipmentList((prev) =>
+      prev.map((equipment) =>
+        equipment._id === equipmentId ? updatedEquipmentData : equipment
+      )
+    );
     toast.success("Equipment updated successfully");
     navigate(-1);
+  };
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImageUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
+    setSelectedFile(file);
+  };
+
+  const handleImageInputClick = () => {
+    imgInputRef.current.click();
+  };
+
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+    // console.log(formData);
+    // console.log(selectedFile);
+
+    const response = await fetch("http://localhost:3000/uploads", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await response.json();
+    // console.log(data);
+
+    setImageId(data);
   };
 
   return (
@@ -69,25 +147,78 @@ const EquipmentDetails = () => {
         >
           Delete
         </button>
-        <button type="submit" className="btn btn-sm btn-success ml-2" onClick={update}>
+        <button
+          type="submit"
+          className="btn btn-sm btn-success ml-2"
+          onClick={update}
+        >
           Save
         </button>
       </div>
-        <div>
-          <div className="text-red-500">Status: </div>
-          <select
-            type="text"
-            name="status"
-            value={formData.status}
-            onChange={handleChange}
-            className="select select-bordered max-w-min mt-2"
-          >
-            <option defaultValue="active" value="active">
-              Active
-            </option>
-            <option value="inactive">Inactive</option>
-          </select>
+      {imageUrl && (
+        <div className="flex justify-start items-center gap-10">
+          <img src={imageUrl} alt="Equipment picture" className="w-1/3 mt-4" />
+
+          <div>
+            <button
+              className={
+                !imageId
+                  ? "btn btn-primary btn-sm ring-1 justify-self-start"
+                  : "hidden"
+              }
+              onClick={uploadImage}
+            >
+              Accept and Upload
+            </button>
+          </div>
         </div>
+      )}
+      {images && (
+        <img
+          src={`data:${images.img.contentType};base64,${arrayBufferToBase64(
+            images.img.data.data
+          )}`}
+          alt={images.name}
+          className="w-1/3 mt-4"
+        />
+      )}
+      {!imageId && (
+        <div className="mt-4">
+          <div
+            className={
+              !imageUrl
+                ? "btn btn-sm btn-primary ring-1 justify-self-start"
+                : "btn btn-sm justify-self-start"
+            }
+            onClick={handleImageInputClick}
+          >
+            Select An Image
+            <input
+              ref={imgInputRef}
+              type="file"
+              onChange={handleImageChange}
+              style={{ display: "none" }}
+              accept="image/*"
+            />
+          </div>
+        </div>
+      )}
+
+      <div>
+        <div className="text-red-500">Status: </div>
+        <select
+          type="text"
+          name="status"
+          value={formData.status}
+          onChange={handleChange}
+          className="select select-bordered max-w-min mt-2"
+        >
+          <option defaultValue="active" value="active">
+            Active
+          </option>
+          <option value="inactive">Inactive</option>
+        </select>
+      </div>
       <div className="flex flex-wrap gap-4 mt-4">
         <div>
           <div>Name: </div>
