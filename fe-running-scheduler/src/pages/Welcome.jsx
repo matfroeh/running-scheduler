3df856996a3ad70dd1cddc6b7ce2a0d5e95e4309
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-11-06 12:09:58 +0100

    final edits

--- fe-running-scheduler/src/pages/Welcome.jsx
+++ fe-running-scheduler/src/pages/Welcome.jsx
@@ -9,7 +9,7 @@ const WelcomePage = () => {
     <>
       {navigation.state === "loading" && (
         <div className="flex justify-center text-nowrap h-full w-full mt-32">
-          <LoadingOverlay active={true} spinner text="Logging in..." />
+          <LoadingOverlay active={true} spinner text="Loading..." />
         </div>
       )}
       <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 text-center">
