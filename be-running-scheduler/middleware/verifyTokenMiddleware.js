a165e40541e225ebd426f496c4034bd903afd24f
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-11-09 22:28:24 +0100

    logging auth

--- be-running-scheduler/middleware/verifyTokenMiddleware.js
+++ be-running-scheduler/middleware/verifyTokenMiddleware.js
@@ -6,6 +6,10 @@ const verifyTokenMiddleware = asyncHandler(async (req, res, next) => {
   try {
     // const { headers: { authorization }} = req;
     let cookie = req.headers.cookie;
+    console.log("Headers:", req.headers);
+    console.log("Request:", req);
+    
+    
     console.log("Cookie:", cookie);
     if (!cookie) throw new ErrorResponse("Unauthorized", 401);
 
