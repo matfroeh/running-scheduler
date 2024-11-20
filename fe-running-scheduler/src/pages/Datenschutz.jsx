e7a323c04283f1743cae2c8edefd6e5e10ebac99
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-11-20 11:40:09 +0100

    dsvgo, footer

--- fe-running-scheduler/src/pages/Datenschutz.jsx
+++ fe-running-scheduler/src/pages/Datenschutz.jsx
@@ -2,7 +2,18 @@ const Datenschutz = () => {
   return (
     <div className="p-6 bg-base-200 text-base-content">
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold mb-4">Datenschutzerklärung</h1>
+        <h1 className="text-2xl font-bold mb-6">Datenschutzerklärung</h1>
+
+        <section className="mb-6">
+          <h2 className="text-xl font-semibold mb-2">Einleitung</h2>
+          <p>
+            Diese Datenschutzerklärung gilt für die private Website
+            runningjournal.online, die von Matthias Fröhlich betrieben wird. Als
+            Privatperson erhebe ich und verarbeite personenbezogene Daten, um
+            die Funktionen meiner Website zu ermöglichen und die
+            Benutzererfahrung zu verbessern.
+          </p>
+        </section>
 
         <section className="mb-6">
           <h2 className="text-xl font-semibold mb-2">1. Verantwortlicher</h2>
@@ -11,18 +22,16 @@ const Datenschutz = () => {
             Sinne der Datenschutz-Grundverordnung (DSGVO) ist:
           </p>
           <p className="mt-2">
-            <strong>[Dein Name/Unternehmen]</strong> <br />
-            [Beispiel: Musterstraße 1] <br />
-            [12345 Musterstadt, Deutschland] <br />
+            <strong>Matthias Fröhlich</strong> <br />
+            Bäckerstr. 26 <br />
+            81241 München, Deutschland <br />
             E-Mail:{" "}
             <a
-              href="mailto:max.mustermann@example.com"
+              href="mailto:froehlich_matthias@yahoo.com"
               className="text-primary"
             >
-              max.mustermann@example.com
+              froehlich_matthias@yahoo.com
             </a>{" "}
-            <br />
-            Telefon: +49 123 456789
           </p>
         </section>
 
@@ -34,8 +43,8 @@ const Datenschutz = () => {
             Wir verarbeiten die folgenden personenbezogenen Daten:
             <ul className="list-disc list-inside mt-2">
               <li>
-                E-Mail-Adresse (bei Registrierung oder Nutzung der
-                Demofunktion).
+                E-Mail-Adresse (bei Registrierung und Nutzung der
+                Webseite).
               </li>
             </ul>
           </p>
@@ -52,17 +61,17 @@ const Datenschutz = () => {
           <ul className="list-disc list-inside mt-2">
             <li>
               <strong>
-                Bereitstellung eines Nutzerkontos oder der Demofunktion:
+                Bereitstellung eines Nutzerkontos:
               </strong>{" "}
               Die Verarbeitung erfolgt zur Erfüllung eines Vertrags oder
               vorvertraglicher Maßnahmen gemäß Art. 6 Abs. 1 lit. b DSGVO.
             </li>
-            <li>
+            {/* <li>
               <strong>Kommunikation und Benachrichtigungen:</strong> Wir
               verwenden Ihre E-Mail-Adresse, um Sie über wichtige Änderungen in
               der App oder technische Probleme zu informieren, auf Grundlage
               Ihrer Einwilligung gemäß Art. 6 Abs. 1 lit. a DSGVO.
-            </li>
+            </li> */}
           </ul>
         </section>
 
@@ -72,7 +81,7 @@ const Datenschutz = () => {
           </h2>
           <p>
             Ihre Daten werden auf Servern unseres Hosting-Anbieters{" "}
-            <strong>[MongoDB Atlas]</strong> gespeichert. Dieser Anbieter
+            <strong>MongoDB Atlas</strong> gespeichert. Dieser Anbieter
             erfüllt die Anforderungen der DSGVO, und die Server befinden sich
             innerhalb der Europäischen Union.
           </p>
@@ -82,7 +91,7 @@ const Datenschutz = () => {
             B. für:
           </p>
           <ul className="list-disc list-inside mt-2">
-            <li>Versand von E-Mails (z. B. durch [Mailgun/AWS SES]).</li>
+            <li>Versand von E-Mails.</li>
           </ul>
         </section>
 
@@ -138,10 +147,10 @@ const Datenschutz = () => {
           <p>
             Zur Ausübung Ihrer Rechte können Sie uns jederzeit unter{" "}
             <a
-              href="mailto:max.mustermann@example.com"
+              href="mailto:froehlich_matthias@yahoo.com"
               className="text-primary"
             >
-              max.mustermann@example.com
+              froehlich_matthias@yahoo.com
             </a>{" "}
             kontaktieren.
           </p>
@@ -155,10 +164,10 @@ const Datenschutz = () => {
             Sie können eine erteilte Einwilligung jederzeit mit Wirkung für die
             Zukunft widerrufen, z. B. durch eine Nachricht an{" "}
             <a
-              href="mailto:max.mustermann@example.com"
+              href="mailto:froehlich_matthias@yahoo.com"
               className="text-primary"
             >
-              max.mustermann@example.com
+              froehlich_matthias@yahoo.com
             </a>
             .
           </p>
@@ -171,15 +180,7 @@ const Datenschutz = () => {
           <p>
             Wenn Sie der Ansicht sind, dass die Verarbeitung Ihrer Daten gegen
             die DSGVO verstößt, können Sie sich bei der zuständigen
-            Datenschutzaufsichtsbehörde beschweren. In Deutschland ist dies z.
-            B.:
-          </p>
-          <p>
-            <strong>
-              Der Landesbeauftragte für Datenschutz und Informationsfreiheit
-              [dein Bundesland]
-            </strong>
-            Adresse und Kontaktdaten je nach Bundesland.
+            Datenschutzaufsichtsbehörde beschweren.
           </p>
         </section>
 
@@ -197,3 +198,37 @@ const Datenschutz = () => {
 };
 
 export default Datenschutz;
+
+// Einleitung
+
+// Rechtsgrundlage
+// Die Verarbeitung personenbezogener Daten erfolgt auf Grundlage von Artikel 6 Absatz 1 Buchstabe f) der Datenschutz-Grundverordnung (DSGVO), da die Verarbeitung erforderlich ist, um die berechtigten Interessen der Webseitenbetreiberin zu wahren.
+
+// Personenbezogene Daten
+// Ich erhebe und verarbeite folgende personenbezogene Daten:
+
+// IP-Adresse des Webseitenbesuchers
+// Benutzername und E-Mail-Adresse (falls angegeben)
+// Cookie-Daten (falls verwendet)
+// Zwecke der Datenverarbeitung
+// Die erhebten und verarbeiteten Daten dienen folgenden Zwecken:
+
+// Verbesserung der Benutzererfahrung durch Analyse von Besucherzahlen und -verhalten
+// Fehlerbehebung und Optimierung von Website-Funktionen
+// Kommunikation mit Besuchern (falls notwendig)
+// Dauer der Datenverarbeitung
+// Die personenbezogenen Daten werden nur so lange gespeichert, wie dies für die oben genannten Zwecke erforderlich ist. Nach Ablauf dieser Frist werden die Daten anonymisiert oder gelöscht.
+
+// Rechte der betroffenen Person
+// Sie haben als betroffene Person folgende Rechte:
+
+// Auskunft über die verarbeiteten Daten
+// Berichtigung von falschen Daten
+// Löschung von Daten (falls keine gesetzlichen Aufbewahrungsfristen bestehen)
+// Einschränkung der Datenverarbeitung (falls eine Löschung nicht möglich ist)
+// Widerspruch gegen die Verarbeitung (falls keine gesetzlichen Voraussetzungen bestehen)
+// Kontakt
+// Wenn Sie Fragen oder Anliegen bezüglich dieser Datenschutzerklärung oder der Datenverarbeitung haben, können Sie sich an mich, [Ihr Name], unter [Kontakt-Email] wenden.
+
+// Aktualisierung
+// Diese Datenschutzerklärung wird regelmäßig überprüft und gegebenenfalls aktualisiert. Die aktuelle Fassung finden Sie immer auf dieser Website.
