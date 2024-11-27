fd4197bacfb13c6509186e652ca21b3a5ed10f58
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-11-27 01:39:16 +0100

    CalendarView / Bar: some additional loading indication when switching calendars and handling cases when no calendars are in db yet

--- fe-running-scheduler/src/components/Calendar/CalendarBar.jsx
+++ fe-running-scheduler/src/components/Calendar/CalendarBar.jsx
@@ -48,82 +48,85 @@ const CalendarBar = ({
   };
 
   return (
-      <div className="flex flex-auto flex-wrap navbar sticky top-0 z-10 bg-base-100">
-        <div className="flex flex-1 navbar-start">
-          <div className="">
-            <ButtonCalendarNavigate
-              text={"Previous"}
-              onClick={showPreviousCalendar}
-              disabled={calendarIndex === 0 || !runs}
-            />
-            <ButtonCalendarNavigate
-              text={"Current"}
-              onClick={showCurrentCalendar}
-              disabled={!runs}
-            />
-            <ButtonCalendarNavigate
-              text={"Next"}
-              onClick={showNextCalendar}
-              disabled={calendarIndex === calendarSize - 1 || !runs}
-            />
-          </div>
-
-          <div className="px-4">
-            {!isLoading ? (
-              <ButtonHiddenInput
-                text={"Upload .gpx"}
-                accept=".gpx"
-                onClick={handleGpxInputClick}
-                onChange={handleGpxFileChange}
-                disabled={!title ? true : false}
-                refForward={gpxInputRef}
-              />
-            ) : (
-              <ButtonLoadingState text={"Processing..."} />
-            )}
-          </div>
+    <div className="flex flex-auto flex-wrap navbar sticky top-0 z-10 bg-base-100">
+      <div className="flex flex-1 navbar-start">
+        <div className="">
+          <ButtonCalendarNavigate
+            text={"Previous"}
+            onClick={showPreviousCalendar}
+            disabled={calendarIndex === 0 || !runs}
+          />
+          <ButtonCalendarNavigate
+            text={"Current"}
+            onClick={showCurrentCalendar}
+            disabled={!runs}
+          />
+          <ButtonCalendarNavigate
+            text={"Next"}
+            onClick={showNextCalendar}
+            disabled={calendarIndex === calendarSize - 1 || !runs}
+          />
         </div>
 
-        <div className="flex flex-1 navbar-center">
-          <div className="flex flex-1  w-1/2 justify-around ">
-            <div className="flex">
-              {title ? (
-                <span
-                  onClick={handleCalendarEdit}
-                  className="border border-accent cursor-pointer rounded-md p-1"
-                >
-                  {title}
-                </span>
-              ) : // <span className="rounded-md p-1">{INITIAL_TITLE}</span>
-              null}
-              {calendarSize === 0 && (
-                <span className="rounded-md p-1">{INITIAL_TITLE}</span>
-              )}
+        <div className="px-4">
+          {!isLoading ? (
+            <ButtonHiddenInput
+              text={"Upload .gpx"}
+              accept=".gpx"
+              onClick={handleGpxInputClick}
+              onChange={handleGpxFileChange}
+              disabled={!title ? true : false}
+              refForward={gpxInputRef}
+            />
+          ) : (
+            <ButtonLoadingState text={"Processing..."} />
+          )}
+        </div>
+      </div>
 
-              <div className="group relative">
-                <button
-                  className="btn btn-sm btn-circle mx-4 hover:ring-1 ring-accent"
-                  onClick={openCreateTrainingBlockModal}
-                >
-                  +
-                </button>
-                <span
-                  className="pointer-events-none absolute left-6 -top-6 text-sm w-max 
+      <div className="flex flex-1 navbar-center">
+        <div className="flex flex-1  w-1/2 justify-around ">
+          <div className="flex">
+            {title ? (
+              <span
+                onClick={handleCalendarEdit}
+                className="border border-accent cursor-pointer rounded-md p-1"
+              >
+                {title}
+              </span>
+            ) : (
+              calendarSize !== 0 && (
+                <span className="rounded-md p-1">Loading...</span>
+              )
+            )}
+            {calendarSize === 0 && (
+              <span className="rounded-md p-1">{INITIAL_TITLE}</span>
+            )}
+
+            <div className="group relative">
+              <button
+                className="btn btn-sm btn-circle mx-4 hover:ring-1 ring-accent"
+                onClick={openCreateTrainingBlockModal}
+              >
+                +
+              </button>
+              <span
+                className="pointer-events-none absolute left-6 -top-6 text-sm w-max 
           opacity-0 transition-opacity duration-700 bg-base-100 group-hover:opacity-100"
-                >
-                  {NEW_TRAINING_SCHEDULE}
-                </span>
-              </div>
+              >
+                {NEW_TRAINING_SCHEDULE}
+              </span>
             </div>
           </div>
         </div>
-        <div className="flex flex-1 navbar-end">
-          <span className="flex ">
-            <ButtonToggle text={"Show Notes"} onClick={toggleNotes} />
-            <ButtonToggle text={"Hide Schedule"} onClick={toggleSchedule} />
-          </span>
-        </div>
       </div>
+      <div className="flex flex-1 navbar-end">
+        <span className="flex ">
+          <ButtonToggle text={"Show Notes"} onClick={toggleNotes} />
+          <ButtonToggle text={"Hide Schedule"} onClick={toggleSchedule} />
+        </span>
+      </div>
+    </div>
   );
 };
 
