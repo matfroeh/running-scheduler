c3de31b3059cc721794ea364f798a24778f6c843
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-12-16 13:36:59 +0100

    back to previous route definition through objects as it will be easier for Framework Adoption from RouterProvider using the React Router Vite plugin

--- fe-running-scheduler/src/App.jsx
+++ fe-running-scheduler/src/App.jsx
@@ -1,9 +1,4 @@
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Navigate,
-    Route,
-} from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
@@ -35,95 +30,110 @@ import { AuthContextProvider } from "@/context";
 // For handling data fetching
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <>
-            <Route path="/" element={<Welcome />}>
-                <Route
-                    path="login"
-                    element={<Login />}
-                    errorElement={<ErrorPage />}
-                />
-                <Route path="signup" element={<SignUp />} />
-            </Route>
-            <Route path="datenschutz" element={<Datenschutz />} />
-            <Route path="impressum" element={<Impressum />} />
-            <Route path="error" element={<ErrorPage />} />
-            <Route path="*" element={<NotFound />} />
-            <Route
-                path="/auth"
-                element={<AuthLayout />}
-                errorElement={<ErrorPage />}
-            >
-                <Route
-                    path="/auth"
-                    element={<RootLayout />}
-                    loader={calendarIndexLoader}
-                    // This is momentarily necessary, because the way of providing a loading feedback upon loading CalendarView and Overview will cause constant reloading of the data.
-                    shouldRevalidate={() => false}
-                    errorElement={<ErrorPage />}
-                >
-                    <Route
-                        index
-                        element={<Navigate to="../calendar" replace />}
-                    />
-                    <Route
-                        path="calendar/:calendarId?"
-                        element={<CalendarView />}
-                        action={getFormData}
-                        errorElement={<ErrorPage />}
-                    >
-                        <Route
-                            path="runs/:week/:day/:runId"
-                            element={<RunDetailsModal />}
-                        />
-
-                        <Route
-                            path="schedule/:week/:day/:trainingDayId"
-                            element={<TrainingDayDetailsModal />}
-                        />
-
-                        <Route
-                            path="new-schedule"
-                            element={<CreateScheduleModal />}
-                        />
-
-                        <Route
-                            path="edit-schedule"
-                            element={<CalendarEditModal />}
-                        />
-                    </Route>
-
-                    <Route
-                        path="overview"
-                        element={<Overview />}
-                        errorElement={<ErrorPage />}
-                        loader={overviewLoader(queryClient)}
-                    />
-
-                    <Route
-                        path="equipment"
-                        element={<EquipmentModal />}
-                        errorElement={<ErrorPage />}
-                    >
-                        <Route
-                            path=":equipmentId"
-                            element={<EquipmentDetails />}
-                        />
-
-                        <Route path="new" element={<CreateEquipment />} />
-                    </Route>
-
-                    <Route
-                        path="profile"
-                        element={<Profile />}
-                        errorElement={<ErrorPage />}
-                    />
-                </Route>
-            </Route>
-        </>
-    )
-);
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Welcome />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "login",
+                element: <Login />,
+            },
+            {
+                path: "signup",
+                element: <SignUp />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+    {
+        path: "/auth",
+        element: <AuthLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/auth",
+                element: <RootLayout />,
+                loader: calendarIndexLoader,
+                // This is momentarily necessary, because the way of providing a loading feedback upon loading CalendarView and Overview will cause constant reloading of the data.
+                shouldRevalidate: () => {
+                    return false;
+                },
+                errorElement: <ErrorPage />,
+                children: [
+                    {
+                        index: true,
+                        element: <Navigate to={"calendar"} replace />,
+                    },
+                    {
+                        path: "calendar/:calendarId?",
+                        element: <CalendarView />,
+                        action: getFormData,
+                        errorElement: <ErrorPage />,
+                        children: [
+                            {
+                                path: "runs/:week/:day/:runId",
+                                element: <RunDetailsModal />,
+                            },
+                            {
+                                path: "schedule/:week/:day/:trainingDayId",
+                                element: <TrainingDayDetailsModal />,
+                            },
+                            {
+                                path: "new-schedule",
+                                element: <CreateScheduleModal />,
+                            },
+                            {
+                                path: "edit-schedule",
+                                element: <CalendarEditModal />,
+                            },
+                        ],
+                    },
+                    {
+                        path: "overview",
+                        element: <Overview />,
+                        loader: overviewLoader(queryClient),
+                        errorElement: <ErrorPage />,
+                    },
+                    {
+                        path: "equipment",
+                        element: <EquipmentModal />,
+                        errorElement: <ErrorPage />,
+                        children: [
+                            {
+                                path: ":equipmentId",
+                                element: <EquipmentDetails />,
+                            },
+                            {
+                                path: "new",
+                                element: <CreateEquipment />,
+                            },
+                        ],
+                    },
+                    {
+                        path: "profile",
+                        element: <Profile />,
+                        errorElement: <ErrorPage />,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        path: "datenschutz",
+        element: <Datenschutz />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "impressum",
+        element: <Impressum />,
+        errorElement: <ErrorPage />,
+    },
+]);
 
 function App() {
     return (
@@ -140,107 +150,92 @@ function App() {
 
 export default App;
 
-// const router = createBrowserRouter([
-//     {
-//         path: "/",
-//         element: <Welcome />,
-//         errorElement: <ErrorPage />,
-//         children: [
-//             {
-//                 path: "login",
-//                 element: <Login />,
-//             },
-//             {
-//                 path: "signup",
-//                 element: <SignUp />,
-//             },
-//         ],
-//     },
-//     {
-//         path: "*",
-//         element: <NotFound />,
-//     },
-//     {
-//         path: "/auth",
-//         element: <AuthLayout />,
-//         errorElement: <ErrorPage />,
-//         children: [
-//             {
-//                 path: "/auth",
-//                 element: <RootLayout />,
-//                 loader: calendarIndexLoader,
-//                 // This is momentarily necessary, because the way of providing a loading feedback upon loading CalendarView and Overview will cause constant reloading of the data.
-//                 shouldRevalidate: () => {
-//                     return false;
-//                 },
-//                 errorElement: <ErrorPage />,
-//                 children: [
-//                     {
-//                         index: true,
-//                         element: <Navigate to={"calendar"} replace />,
-//                     },
-//                     {
-//                         path: "calendar/:calendarId?",
-//                         element: <CalendarView />,
-//                         action: getFormData,
-//                         errorElement: <ErrorPage />,
-//                         children: [
-//                             {
-//                                 path: "runs/:week/:day/:runId",
-//                                 element: <RunDetailsModal />,
-//                             },
-//                             {
-//                                 path: "schedule/:week/:day/:trainingDayId",
-//                                 element: <TrainingDayDetailsModal />,
-//                             },
-//                             {
-//                                 path: "new-schedule",
-//                                 element: <CreateScheduleModal />,
-//                             },
-//                             {
-//                                 path: "edit-schedule",
-//                                 element: <CalendarEditModal />,
-//                             },
-//                         ],
-//                     },
-//                     {
-//                         path: "overview",
-//                         element: <Overview />,
-//                         loader: overviewLoader(queryClient),
-//                         errorElement: <ErrorPage />,
-//                     },
-//                     {
-//                         path: "equipment",
-//                         element: <EquipmentModal />,
-//                         errorElement: <ErrorPage />,
-//                         children: [
-//                             {
-//                                 path: ":equipmentId",
-//                                 element: <EquipmentDetails />,
-//                             },
-//                             {
-//                                 path: "new",
-//                                 element: <CreateEquipment />,
-//                             },
-//                         ],
-//                     },
-//                     {
-//                         path: "profile",
-//                         element: <Profile />,
-//                         errorElement: <ErrorPage />,
-//                     },
-//                 ],
-//             },
-//         ],
-//     },
-//     {
-//         path: "datenschutz",
-//         element: <Datenschutz />,
-//         errorElement: <ErrorPage />,
-//     },
-//     {
-//         path: "impressum",
-//         element: <Impressum />,
-//         errorElement: <ErrorPage />,
-//     },
-// ]);
+// const router = createBrowserRouter(
+//     createRoutesFromElements(
+//         <>
+//             <Route path="/" element={<Welcome />}>
+//                 <Route
+//                     path="login"
+//                     element={<Login />}
+//                     errorElement={<ErrorPage />}
+//                 />
+//                 <Route path="signup" element={<SignUp />} />
+//             </Route>
+//             <Route path="datenschutz" element={<Datenschutz />} />
+//             <Route path="impressum" element={<Impressum />} />
+//             <Route path="error" element={<ErrorPage />} />
+//             <Route path="*" element={<NotFound />} />
+//             <Route
+//                 path="/auth"
+//                 element={<AuthLayout />}
+//                 errorElement={<ErrorPage />}
+//             >
+//                 <Route
+//                     path="/auth"
+//                     element={<RootLayout />}
+//                     loader={calendarIndexLoader}
+//                     // This is momentarily necessary, because the way of providing a loading feedback upon loading CalendarView and Overview will cause constant reloading of the data.
+//                     shouldRevalidate={() => false}
+//                     errorElement={<ErrorPage />}
+//                 >
+//                     <Route
+//                         index
+//                         element={<Navigate to="../calendar" replace />}
+//                     />
+//                     <Route
+//                         path="calendar/:calendarId?"
+//                         element={<CalendarView />}
+//                         action={getFormData}
+//                         errorElement={<ErrorPage />}
+//                     >
+//                         <Route
+//                             path="runs/:week/:day/:runId"
+//                             element={<RunDetailsModal />}
+//                         />
+
+//                         <Route
+//                             path="schedule/:week/:day/:trainingDayId"
+//                             element={<TrainingDayDetailsModal />}
+//                         />
+
+//                         <Route
+//                             path="new-schedule"
+//                             element={<CreateScheduleModal />}
+//                         />
+
+//                         <Route
+//                             path="edit-schedule"
+//                             element={<CalendarEditModal />}
+//                         />
+//                     </Route>
+
+//                     <Route
+//                         path="overview"
+//                         element={<Overview />}
+//                         errorElement={<ErrorPage />}
+//                         loader={overviewLoader(queryClient)}
+//                     />
+
+//                     <Route
+//                         path="equipment"
+//                         element={<EquipmentModal />}
+//                         errorElement={<ErrorPage />}
+//                     >
+//                         <Route
+//                             path=":equipmentId"
+//                             element={<EquipmentDetails />}
+//                         />
+
+//                         <Route path="new" element={<CreateEquipment />} />
+//                     </Route>
+
+//                     <Route
+//                         path="profile"
+//                         element={<Profile />}
+//                         errorElement={<ErrorPage />}
+//                     />
+//                 </Route>
+//             </Route>
+//         </>
+//     )
+// );
