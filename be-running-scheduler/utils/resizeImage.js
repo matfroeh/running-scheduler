c674e4b40b6c4063bdb9671c975707e90661d0d2
Matthias Froehlich <froehlich_matthias@yahoo.com>
2025-02-03 10:42:13 +0100

    fix for unexpected image rotation of equipment images during resizing

--- be-running-scheduler/utils/resizeImage.js
+++ be-running-scheduler/utils/resizeImage.js
@@ -2,6 +2,9 @@ import sharp from "sharp";
 
 export const resizeImage = async (buffer, width, height) => {
   const image = await sharp(buffer);
-  const resizedImage = await image.resize(width, height).jpeg({ quality: 80 });
+  const resizedImage = await image
+    .resize(width, height)
+    .jpeg({ quality: 80 })
+    .rotate(null);
   return resizedImage.toBuffer();
 };
