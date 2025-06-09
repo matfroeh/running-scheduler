143a121851be83c06b53ea918b1104df2125b7ed
Matthias Froehlich <froehlich_matthias@yahoo.com>
2025-06-09 13:07:11 +0200

    Weekly Summaries and Overview: Workout Types [Rest Day, Strenght Training, Cross Training] are now excluded for the calculation of the summaries

--- fe-running-scheduler/src/lib/calculateSummariesForOverview.js
+++ fe-running-scheduler/src/lib/calculateSummariesForOverview.js
@@ -1,69 +1,98 @@
+import { checkForExcludedWorkoutType } from "@/lib";
+
 export const calculateSummariesForOverview = (runningWeek) => {
-  const totalDistanceRun = Object.keys(runningWeek.days).reduce((acc, day) => {
-    if (runningWeek.days[day].distance) {
-      acc += parseFloat(runningWeek.days[day].distance);
-    }
-    return parseFloat(acc);
-  }, 0);
+    const totalDistanceRun = Object.keys(runningWeek.days).reduce(
+        (acc, day) => {
+            if (
+                checkForExcludedWorkoutType(runningWeek.days[day]) ||
+                !runningWeek.days[day].distance
+            ) {
+                return acc; //
+            }
+            if (runningWeek.days[day].distance) {
+                acc += parseFloat(runningWeek.days[day].distance);
+            }
+            return parseFloat(acc);
+        },
+        0
+    );
 
-  const totalTime = Object.keys(runningWeek.days).reduce((acc, day) => {
-    if (runningWeek.days[day].duration > 0) {
-      acc += parseInt(runningWeek.days[day].duration);
-    }
-    return acc;
-  }, 0);
-
-  // time weighted average heart rate
-  const getAvgHr = () => {
-    let avg_hr = Object.keys(runningWeek.days).reduce(
-      (acc, day) => {
-        if (parseInt(runningWeek.days[day].avg_hr)) {
-          acc.days += 1;
-          acc.hr +=
-            parseInt(runningWeek.days[day].avg_hr) *
-            parseInt(runningWeek.days[day].duration);
+    const totalTime = Object.keys(runningWeek.days).reduce((acc, day) => {
+        if (
+            checkForExcludedWorkoutType(runningWeek.days[day]) ||
+            !runningWeek.days[day].distance
+        ) {
+            return acc; //
+        }
+        if (runningWeek.days[day].duration > 0) {
+            acc += parseInt(runningWeek.days[day].duration);
         }
         return acc;
-      },
-      { days: 0, hr: 0 }
-    );
-    if (avg_hr.days > 0) {
-      return avg_hr.hr / totalTime;
-      // return avg_hr.hr / avg_hr.days;
-    }
-    return null;
-  };
+    }, 0);
 
-  // time weighted average effort
-  const getEffort = () => {
-    let totalEffort = Object.keys(runningWeek.days).reduce(
-      (acc, day) => {
-        if (runningWeek.days[day].effort) {
-          acc.days += 1;
-          acc.effort +=
-            parseInt(runningWeek.days[day].effort) *
-            parseInt(runningWeek.days[day].duration);
+    // time weighted average heart rate
+    const getAvgHr = () => {
+        let avg_hr = Object.keys(runningWeek.days).reduce(
+            (acc, day) => {
+                if (
+                    checkForExcludedWorkoutType(runningWeek.days[day]) ||
+                    !runningWeek.days[day].distance
+                ) {
+                    return acc; //
+                }
+                if (parseInt(runningWeek.days[day].avg_hr)) {
+                    acc.days += 1;
+                    acc.hr +=
+                        parseInt(runningWeek.days[day].avg_hr) *
+                        parseInt(runningWeek.days[day].duration);
+                }
+                return acc;
+            },
+            { days: 0, hr: 0 }
+        );
+        if (avg_hr.days > 0) {
+            return avg_hr.hr / totalTime;
+            // return avg_hr.hr / avg_hr.days;
         }
-        return acc;
-      },
-      { days: 0, effort: 0 }
-    );
-    if (totalEffort.days > 0) {
-      return totalEffort.effort / totalTime;
-      // return totalEffort.effort / totalEffort.days
-    }
-    return null;
-  };
+        return null;
+    };
 
-  const avgPace = parseFloat(totalTime / 60 / totalDistanceRun).toFixed(2);
-  const avg_hr = getAvgHr();
-  const avgEffort = getEffort();
+    // time weighted average effort
+    const getEffort = () => {
+        let totalEffort = Object.keys(runningWeek.days).reduce(
+            (acc, day) => {
+                if (
+                    checkForExcludedWorkoutType(runningWeek.days[day]) ||
+                    !runningWeek.days[day].distance
+                ) {
+                    return acc; //
+                }
+                if (runningWeek.days[day].effort) {
+                    acc.days += 1;
+                    acc.effort +=
+                        parseInt(runningWeek.days[day].effort) *
+                        parseInt(runningWeek.days[day].duration);
+                }
+                return acc;
+            },
+            { days: 0, effort: 0 }
+        );
+        if (totalEffort.days > 0) {
+            return totalEffort.effort / totalTime;
+            // return totalEffort.effort / totalEffort.days
+        }
+        return null;
+    };
 
-  return {
-    totalDistanceRun,
-    totalTime,
-    avgPace,
-    avg_hr,
-    avgEffort,
-  };
+    const avgPace = parseFloat(totalTime / 60 / totalDistanceRun).toFixed(2);
+    const avg_hr = getAvgHr();
+    const avgEffort = getEffort();
+
+    return {
+        totalDistanceRun,
+        totalTime,
+        avgPace,
+        avg_hr,
+        avgEffort,
+    };
 };
--- fe-running-scheduler/src/lib/calculateWeeklySummary.js
+++ fe-running-scheduler/src/lib/calculateWeeklySummary.js
@@ -1,82 +1,103 @@
 import { getSecondsAsHoursMinutesSecondsString } from "@/lib/utils";
+import { checkForExcludedWorkoutType } from "@/lib";
 
 export const calculateWeeklySummary = (scheduleWeek, runningWeek) => {
-  const totalDistancePlanned = Object.keys(scheduleWeek.days).reduce(
-    (acc, day) => {
-      if (scheduleWeek.days[day].distance) {
-        acc += parseFloat(scheduleWeek.days[day].distance);
-      }
-      return Math.round(parseFloat(acc));
-    },
-    0
-  );
+    const totalDistancePlanned = Object.keys(scheduleWeek.days).reduce(
+        (acc, day) => {
+            if (
+                checkForExcludedWorkoutType(
+                    scheduleWeek.days[day] || !scheduleWeek.days[day].distance
+                )
+            ) {
+                return acc;
+            }
+            return (acc += parseFloat(scheduleWeek.days[day].distance));
+        },
+        0
+    );
 
-  const totalDistanceRun = Object.keys(runningWeek.days).reduce((acc, day) => {
-    if (runningWeek.days[day].distance) {
-      acc += parseFloat(runningWeek.days[day].distance);
-    }
-    return parseFloat(acc);
-  }, 0);
+    const totalDistanceRun = Object.keys(runningWeek.days).reduce(
+        (acc, day) => {
+            if (
+                checkForExcludedWorkoutType(runningWeek.days[day]) ||
+                !runningWeek.days[day].distance
+            ) {
+                return acc; //
+            }
+            return acc + parseFloat(runningWeek.days[day].distance);
+        },
+        0
+    );
 
-  const totalTime = Object.keys(runningWeek.days).reduce((acc, day) => {
-    if (runningWeek.days[day].duration > 0) {
-      acc += parseInt(runningWeek.days[day].duration);
-    }
-    return acc;
-  }, 0);
+    const totalTime = Object.keys(runningWeek.days).reduce((acc, day) => {
+        if (checkForExcludedWorkoutType(runningWeek.days[day])) {
+            return acc;
+        }
 
-  // time weighted average heart rate
-  const getAvgHr = () => {
-    let avg_hr = Object.keys(runningWeek.days).reduce(
-      (acc, day) => {
-        if (parseInt(runningWeek.days[day].avg_hr)) {
-          acc.days += 1;
-          acc.hr +=
-            parseInt(runningWeek.days[day].avg_hr) *
-            parseInt(runningWeek.days[day].duration);
+        if (runningWeek.days[day].duration > 0) {
+            acc += parseInt(runningWeek.days[day].duration);
         }
         return acc;
-      },
-      { days: 0, hr: 0 }
-    );
-    if (avg_hr.days > 0) {
-      return avg_hr.hr / totalTime;
-    }
-    return null;
-  };
+    }, 0);
 
-  // time weighted average effort
-  const getEffort = () => {
-    let totalEffort = Object.keys(runningWeek.days).reduce(
-      (acc, day) => {
-        if (runningWeek.days[day].effort) {
-          acc.days += 1;
-          acc.effort +=
-            parseInt(runningWeek.days[day].effort) *
-            parseInt(runningWeek.days[day].duration);
+    // time weighted average heart rate
+    const getAvgHr = () => {
+        let avg_hr = Object.keys(runningWeek.days).reduce(
+            (acc, day) => {
+                if (checkForExcludedWorkoutType(runningWeek.days[day])) {
+                    return acc;
+                }
+                if (parseInt(runningWeek.days[day].avg_hr)) {
+                    acc.days += 1;
+                    acc.hr +=
+                        parseInt(runningWeek.days[day].avg_hr) *
+                        parseInt(runningWeek.days[day].duration);
+                }
+                return acc;
+            },
+            { days: 0, hr: 0 }
+        );
+        if (avg_hr.days > 0) {
+            return avg_hr.hr / totalTime;
         }
-        return acc;
-      },
-      { days: 0, effort: 0 }
-    );
-    if (totalEffort.days > 0) {
-      return totalEffort.effort / totalTime;
-    }
-    return null;
-  };
+        return null;
+    };
 
-  const totalTimeFormatted = getSecondsAsHoursMinutesSecondsString(totalTime);
-  const avgPace = parseFloat(totalTime / 60 / totalDistanceRun).toFixed(2);
-  const avg_hr = getAvgHr();
-  const avgEffort = getEffort();
+    // time weighted average effort
+    const getEffort = () => {
+        let totalEffort = Object.keys(runningWeek.days).reduce(
+            (acc, day) => {
+                if (checkForExcludedWorkoutType(runningWeek.days[day])) {
+                    return acc;
+                }
+                if (runningWeek.days[day].effort) {
+                    acc.days += 1;
+                    acc.effort +=
+                        parseInt(runningWeek.days[day].effort) *
+                        parseInt(runningWeek.days[day].duration);
+                }
+                return acc;
+            },
+            { days: 0, effort: 0 }
+        );
+        if (totalEffort.days > 0) {
+            return totalEffort.effort / totalTime;
+        }
+        return null;
+    };
 
-  return {
-    totalDistancePlanned,
-    totalDistanceRun,
-    totalTime,
-    totalTimeFormatted,
-    avgPace,
-    avg_hr,
-    avgEffort,
-  };
+    const totalTimeFormatted = getSecondsAsHoursMinutesSecondsString(totalTime);
+    const avgPace = parseFloat(totalTime / 60 / totalDistanceRun).toFixed(2);
+    const avg_hr = getAvgHr();
+    const avgEffort = getEffort();
+
+    return {
+        totalDistancePlanned,
+        totalDistanceRun,
+        totalTime,
+        totalTimeFormatted,
+        avgPace,
+        avg_hr,
+        avgEffort,
+    };
 };
