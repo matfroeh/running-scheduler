a83c09adbf6de12256379ee971866ff20f49856d
Matthias Froehlich <froehlich_matthias@yahoo.com>
2025-06-12 11:27:11 +0200

    Whether the Schedule is shown on default or not is now a Local storage saved user preference; +minor UI color change

--- fe-running-scheduler/src/components/CalendarView/CardContainer.jsx
+++ fe-running-scheduler/src/components/CalendarView/CardContainer.jsx
@@ -1,17 +1,17 @@
 const CardContainer = ({ isToday, openRunCard, children }) => {
-  // const baseClass =
-  //   "mx-0.5 max-w-44 min-w-20 card card-compact rounded-tr-none rounded-br-none rounded-tl-none " +
-  //   "bg-gray-900 ring-1 hover:ring-2 cursor-pointer";
-  const baseClass =
-  "mx-0.5 max-w-40 min-w-20 card card-compact rounded-tr-none rounded-br-none rounded-tl-none " +
-  "bg-gray-900 ring-1 hover:ring-2 cursor-pointer h-max overflow-clip";
-  const todayClass = isToday ? "ring-green-500" : "";
+    // const baseClass =
+    //   "mx-0.5 max-w-44 min-w-20 card card-compact rounded-tr-none rounded-br-none rounded-tl-none " +
+    //   "bg-gray-900 ring-1 hover:ring-2 cursor-pointer";
+    const baseClass =
+        "mx-0.5 max-w-40 min-w-20 card card-compact rounded-tr-none rounded-br-none rounded-tl-none " +
+        "bg-gray-800 ring-1 hover:ring-2 cursor-pointer h-max overflow-clip";
+    const todayClass = isToday ? "ring-green-500" : "";
 
-  return (
-    <div className={`${baseClass} ${todayClass}`} onClick={openRunCard}>
-      {children}
-    </div>
-  );
+    return (
+        <div className={`${baseClass} ${todayClass}`} onClick={openRunCard}>
+            {children}
+        </div>
+    );
 };
 
 export default CardContainer;
--- fe-running-scheduler/src/components/generic/ButtonToggle.jsx
+++ fe-running-scheduler/src/components/generic/ButtonToggle.jsx
@@ -1,16 +1,19 @@
-const ButtonToggle = ({text, onClick}) => {
-  return (
-    <label className="label cursor-pointer">
-    <span className="label-text text-xs md:text-sm text-nowrap">
-      {text}
-    </span>
-    <input
-      type="checkbox"
-      className="toggle toggle-xs sm:toggle-md toggle-accent mx-2"
-      onClick={onClick}
-    />
-  </label>
-  )
-}
+const ButtonToggle = ({ text, onChange, isChecked }) => {
+    // console.log("Toggle State at ButtonToggle.jsx", text, onChange);
 
-export default ButtonToggle
+    return (
+        <label className="label cursor-pointer">
+            <span className="label-text text-xs md:text-sm text-nowrap">
+                {text}
+            </span>
+            <input
+                type="checkbox"
+                checked={isChecked}
+                className="toggle toggle-xs sm:toggle-md toggle-accent mx-2"
+                onChange={onChange}
+            />
+        </label>
+    );
+};
+
+export default ButtonToggle;
